77bb2393038fc8a031ddddef657acc8df9e0b73c
zaliberty <dream_vital@mail.ru>
2014-05-26 21:26:11 +0400

    Add DBContext to HomeDayReports.Repository

---
+++ HomeDayReports/HomeDayReports.Repository/DBContext.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeDayReports.Repository
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Idea> Ideas { get; set; }
+        public DbSet<BusinesIdea> BusinesIdeas { get; set; }
+        public DbSet<DoneTask> DoneTasks { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<DayReport> DayReports { get; set; }
+    }
+}
